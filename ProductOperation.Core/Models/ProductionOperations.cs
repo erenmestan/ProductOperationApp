70d91fbd1c22066bf51c9db03ea159b2a2ea7b7b
erenmestan <ermestan@gmail.com>
2022-04-20 15:23:50 +0300

    Add project files.

---
+++ ProductOperation.Core/Models/ProductionOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductOperation.Core.Models
+{
+    public class ProductionOperations:BaseEntity
+    {
+        public string No { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool Status { get; set; }
+        public string StoppingReason { get; set; }
+
+
+    }
+}
---
+++ ProductOperation.Core/Models/StandartStoppings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductOperation.Core.Models
+{
+    public class StandartStoppings:BaseEntity
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string StoppingReason { get; set; }
+
+    }
+}
